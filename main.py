511f24371a58e333485fe85c39d13e14d3227e15
Amit <heroku467@gmail.com>
2025-04-11 23:05:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,26 @@ import os
 from multiprocessing import Process
 
 def run_bot1():
-    subprocess.call(["git", "clone", "https://github.com/Ur-amit-01/post.git"])
-    os.chdir("post")
+    os.chdir("/app")
+    subprocess.call(["git", "clone", "https://github.com/Ur-amit-01/Post-Manager.git", "Post-Manager-Bot1"])
+    os.chdir("Post-Manager-Bot1")
+    
+    # Set environment variables for bot 1
+    os.environ["BOT_TOKEN"] = "7408399689:AAGyXnXAIeja9qyg7eEPghKkmL4Z4OJy5-w"
+    os.environ["DATABASE_URL"] = ""
+    
     subprocess.call(["pip", "install", "-r", "requirements.txt"])
     subprocess.call(["python", "bot.py"])
 
 def run_bot2():
-    os.chdir("/app")  # Make sure we're back in root
-    subprocess.call(["git", "clone", "https://github.com/Ur-amit-01/Post-Manager.git"])
-    os.chdir("Post-Manager")
+    os.chdir("/app")
+    subprocess.call(["git", "clone", "https://github.com/Ur-amit-01/Post-Manager.git", "Post-Manager-Bot2"])
+    os.chdir("Post-Manager-Bot2")
+
+    # Set environment variables for bot 2
+    os.environ["BOT_TOKEN"] = "8113642693:AAG9yJpZjyhKIP_nhsIIoc8ZsiTJ-gsudLU"
+    os.environ["DATABASE_URL"] = ""
+    
     subprocess.call(["pip", "install", "-r", "requirements.txt"])
     subprocess.call(["python", "bot.py"])
 

dd788a64cb10bb74a7b3de0fa5ff7274459e7872
Amit <heroku467@gmail.com>
2025-04-11 22:22:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,17 @@ import os
 from multiprocessing import Process
 
 def run_bot1():
-    subprocess.call(["git", "clone", "https://github.com/Ur-amit-01/Post-Manager.git"])
-    os.chdir("bot1")
+    subprocess.call(["git", "clone", "https://github.com/Ur-amit-01/post.git"])
+    os.chdir("post")
     subprocess.call(["pip", "install", "-r", "requirements.txt"])
-    subprocess.call(["python", "bot.py"])
+    subprocess.call(["python", "main.py"])
 
 def run_bot2():
-    subprocess.call(["git", "clone", "https://github.com/Ur-amit-01/post.git"])
-    os.chdir("../")  # Back to parent before cloning
-    subprocess.call(["git", "clone", "https://github.com/Ur-amit-01/post.git"])
-    os.chdir("bot2")
+    os.chdir("/app")  # Make sure we're back in root
+    subprocess.call(["git", "clone", "https://github.com/Ur-amit-01/Post-Manager.git"])
+    os.chdir("Post-Manager")
     subprocess.call(["pip", "install", "-r", "requirements.txt"])
-    subprocess.call(["python", "bot.py"])
+    subprocess.call(["python", "main.py"])
 
 if __name__ == "__main__":
     p1 = Process(target=run_bot1)

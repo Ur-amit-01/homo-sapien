dcb03282379ae5600e419a62893e3aebb0ef7180
Amit <heroku467@gmail.com>
2025-04-11 23:29:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,10 +26,25 @@ def run_bot2():
     subprocess.call(["pip", "install", "-r", "requirements.txt"])
     subprocess.call(["python", "bot.py"])
 
+def run_bot3():
+    os.chdir("/app")
+    subprocess.call(["git", "clone", "https://github.com/Ur-amit-01/Post-Manager.git", "Post-Manager-Bot3"])
+    os.chdir("Post-Manager-Bot3")
+
+    # Set environment variables for bot 2
+    os.environ["BOT_TOKEN"] = "7557297602:AAH6-43MKGE0umypUgeonsfk41wOrsDKCnM"
+    os.environ["DATABASE_URL"] = ""
+    
+    subprocess.call(["pip", "install", "-r", "requirements.txt"])
+    subprocess.call(["python", "bot.py"])
+    
 if __name__ == "__main__":
     p1 = Process(target=run_bot1)
     p2 = Process(target=run_bot2)
+    p3 = Process(target=run_bot3)
     p1.start()
     p2.start()
+    p3.start()
     p1.join()
     p2.join()
+    p3.join()

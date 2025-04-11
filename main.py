1b02a942c05cdcd10f14ed5e8ca645e35fd24ee0
Amit <heroku467@gmail.com>
2025-04-11 22:24:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ def run_bot1():
     subprocess.call(["git", "clone", "https://github.com/Ur-amit-01/post.git"])
     os.chdir("post")
     subprocess.call(["pip", "install", "-r", "requirements.txt"])
-    subprocess.call(["python", "main.py"])
+    subprocess.call(["python", "bot.py"])
 
 def run_bot2():
     os.chdir("/app")  # Make sure we're back in root
     subprocess.call(["git", "clone", "https://github.com/Ur-amit-01/Post-Manager.git"])
     os.chdir("Post-Manager")
     subprocess.call(["pip", "install", "-r", "requirements.txt"])
-    subprocess.call(["python", "main.py"])
+    subprocess.call(["python", "bot.py"])
 
 if __name__ == "__main__":
     p1 = Process(target=run_bot1)

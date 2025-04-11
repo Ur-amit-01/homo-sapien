6354990ae5967a4d5b2312807d3256dcb529f056
Amit <heroku467@gmail.com>
2025-04-12 01:01:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ logger = setup_logger('BotLauncher')
 # ====================== BOT CONFIGS ====================== #
 BOT1_CONFIG = {
     "name": "Team SAT",
-    "token": "7408...",
+    "token": "7821967646:AAFHUS91204U6P6xqnBOdAefk42agRWzTc0",
     "db_name": "SAT_manager",
     "admins": "7150972327 2031106491 1519459773",
     "session_string": "BQFP49AAROLIHpH9tfjlrEewakOd-NndU3oDb9F1OJMp3cLaksypCJuuS1Qm8Bz6FjsIXzlyq1C2_Tz4gKj6vz2vZ-bElYZ8-0NLco6I74pWQOi2GQqBfvX9ls8EC3coHPY6YzfkEORGOt-i_Y05fw_UXE1NubilnLt1AOPA25gueZX-j8Jdf4c-gsA4i2qdaVjaWSNMba7F-aZ7W3KEFl3CO0KaVRqwFT9lDXEcZVc_UuYr0FG0f9qOmh7vyo-M7fr6lX7RzJjxR7AONx_9QB9rHWL3cogjbdhR9wGUoT2n7xbMKSJbtZH4L7W8H3NtaUl-svjOTmnHqQ4i11H3gsUMCbxR0QAAAAGVhUI_AA"  # Add session string if needed
